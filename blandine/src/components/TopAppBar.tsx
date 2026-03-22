e8656ceecdb09b73f2fc6f1f58772988ef79ba06
Crimra <smithmounkassa2@gmail.com>
2026-03-22 20:07:36 +0100

    remove: account icon from topappbar

--- blandine/src/components/TopAppBar.tsx
+++ blandine/src/components/TopAppBar.tsx
@@ -31,13 +31,6 @@ export default function TopAppBar({ title = 'PharmaSerene', showBack = false, ac
       </div>
       <div className="flex items-center gap-3">
         {actions}
-        <div className="w-8 h-8 rounded-full overflow-hidden" style={{ backgroundColor: 'var(--surface-secondary)' }}>
-          <img
-            alt="Profile"
-            className="w-full h-full object-cover"
-            src="https://lh3.googleusercontent.com/aida-public/AB6AXuAqz_I3b2YRhD8aTedg_Jg4hUadapVY4VqUjBTl53HPvmptGOnBgwvUHm71icQLfK2Gxvvhgd4G9OGnEM5echyHYUpk8YsWT6Y4jVwhQv_Wxlk8pxe5t4R-u-u8wGdUfXykGyRFyG6DHVNJ6HLMpYhP_BzrLQWWYa0W3-6W1CvZNkfGMlGpcY6_1vUTjUOYxfl0OA-VU-DtcYKeZtQpgkDLhHvzb4d0dwH1RIE410F1E8SpPk4ughzGNMVBBG877RE8KeaZNRzZP6M"
-          />
-        </div>
       </div>
     </header>
   );
